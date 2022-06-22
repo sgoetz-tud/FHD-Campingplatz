cde35a366e1eb5a5b7e6a03675622db84c84f2dd
Sebastian Götz <goetz.jexam@gmail.com>
2022-06-22 11:07:14 +0200

    Produkte im Shop

--- Campingplatz/Program.cs
+++ Campingplatz/Program.cs
@@ -3,6 +3,8 @@
     private List<Stellplatz> stellplaetze;
     private List<Buchung> buchungen;
 
+    public Shop Shop { get; set; }
+
     public Campingplatz()
     {
         stellplaetze = new List<Stellplatz>();
@@ -14,6 +16,8 @@
         stellplaetze.Add(new Standardstellplatz(203));
         stellplaetze.Add(new Zeltparzelle(404));
         stellplaetze.Add(new Zeltparzelle(420));
+
+        Shop = new Shop();
 
         buchungen = new List<Buchung>();
     }
@@ -27,10 +31,17 @@
         return null;
     }
 
-    public int Abrechnen(Buchung b)
+    public double Abrechnen(Buchung b)
     {
         int days = (b.Abreise - b.Anreise).Days;
-        return days * b.Stellplatz.Preis;
+        double preis = days * b.Stellplatz.Preis;
+        
+        foreach(Produkt p in b.GetGekaufteProdukte())
+        {
+            preis += p.Preis;
+        }
+
+        return preis;
     }
 
     static void Main()
@@ -39,7 +50,15 @@
         Kunde kunde = new Kunde();
         Buchung b = kunde.buchen(cp, DateTime.Parse("01.07.2022"), DateTime.Parse("10.07.2022"), 303);
 
-        int preis = cp.Abrechnen(b);
+        foreach(Produkt p in cp.Shop.GetProdukte())
+        {
+            if(p.Name == "Brötchen")
+            {
+                kunde.KaufeProdukt(p, 303);
+            }
+        }
+
+        double preis = cp.Abrechnen(b);
         
         Console.WriteLine("Gesamtkosten: "+preis);
     }
@@ -86,11 +105,24 @@ class Buchung
     public DateTime Anreise { get; set; }
     public DateTime Abreise { get; set; }
     public Stellplatz Stellplatz { get; set; }
+    
+    private List<Produkt> gekaufteProdukte;
     public Buchung(DateTime anreise, DateTime abreise, Stellplatz sp)
     {
         Anreise = anreise;
         Abreise = abreise;
         Stellplatz = sp;
+        gekaufteProdukte = new List<Produkt>();
+    }
+
+    public void ProduktHinzufügen(Produkt p)
+    {
+        gekaufteProdukte.Add(p);
+    }
+
+    public List<Produkt> GetGekaufteProdukte()
+    {
+        return gekaufteProdukte;
     }
 }
 
@@ -110,4 +142,59 @@ class Kunde
         buchungen.Add(b);
         return b;
     }
+
+    public void KaufeProdukt(Produkt p, int nummer)
+    {
+        //Buchung finden
+        Buchung aktuelleBuchung;
+        foreach(Buchung b in buchungen)
+        {
+            if (b.Stellplatz.Nummer == nummer)
+            {
+                aktuelleBuchung = b;
+                aktuelleBuchung.ProduktHinzufügen(p);
+                break;
+            }
+        }
+    }
+}
+
+class Shop
+{
+    private List<Produkt> produkte;
+
+    public Shop()
+    {
+        produkte = new List<Produkt>();
+        produkte.Add(new Produkt("Brötchen",0.50));
+        produkte.Add(new Produkt("Brötchen", 0.50));
+        produkte.Add(new Produkt("Brötchen", 0.50));
+        produkte.Add(new Produkt("Brötchen", 0.50));
+        produkte.Add(new Produkt("Brötchen", 0.50));
+        produkte.Add(new Produkt("Croissant", 0.99));
+        produkte.Add(new Produkt("Croissant", 0.99));
+        produkte.Add(new Produkt("Croissant", 0.99));
+        produkte.Add(new Produkt("Campinggas", 2.00));
+        produkte.Add(new Produkt("Campinggas", 2.00));
+        produkte.Add(new Produkt("Limo", 1.00));
+        produkte.Add(new Produkt("Limo", 1.00));
+    }
+
+    public List<Produkt> GetProdukte()
+    {
+        return produkte;
+    }
+}
+
+class Produkt
+{
+    public string Name { get; set; }
+    public double Preis { get; set; }
+
+    public Produkt(string name, double preis)
+    {
+        Name = name;
+        Preis = preis;
+    }
+
 }
